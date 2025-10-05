70faf065d824d04ae85b8e52029df69915834a0a
Kapil-dhakad <dhakadkapil350@gmail.com>
2025-10-05 09:05:45 +0530

    feat: deploy setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const app = require('./src/app')
 const connectDB = require('./src/db/db')
+const path = require('path');
+const express = require("express")
 
 connectDB()
 
-app.listen(3000, ()=> {
-    console.log("server is running on port 3000")
+app.use(express.static(path.join(__dirname, 'public')));
+
+// React Router fallback
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on port ${PORT}`)
 })
